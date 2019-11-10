628b0af63bfcceb31ec122401132b7ec1559378b
Alek Ratzloff <alekratz@gmail.com>
2019-11-10 14:25:01 -0500

    Add PartialOrd and Ord to common::binding::Binding, and documentation for the struct
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- src/common/binding.rs
+++ src/common/binding.rs
@@ -1,7 +1,13 @@
 use crate::common::prelude::*;
 use shrinkwraprs::Shrinkwrap;
 
-#[derive(Shrinkwrap, Debug, Hash, PartialEq, Eq, Clone, Copy, Default)]
+/// The base name binding type, used throughout the codebase.
+///
+/// This is an index value that is used as a lightweight reference to some local value in the
+/// function stack. Bindings are lexical, so they are assigned during the initial syntax and IR
+/// passes. They are subsequently used to define builtin function names, as well as operators.
+/// Ultimately, they are used in the VM layer as register IDs.
+#[derive(Shrinkwrap, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Default)]
 pub struct Binding(pub usize);
 
 impl Binding {
